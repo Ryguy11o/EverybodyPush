37a6d0795552af87b6451663b7b95df99bbc5000
Ryan <rbo5046@psu.edu>
2018-04-10 14:11:33 -0400

    Ryan added his first push

--- src/everybodypush/EverybodyPush.java
+++ src/everybodypush/EverybodyPush.java
@@ -16,6 +16,7 @@ public class EverybodyPush {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World - Ryan is pushing");
     }
     
 }
